eb98985a54b43ac434abb51c626d791b334f8cba
SeppiaBrilla <seppiabrilla@pop-os.localdomain>
2023-11-28 11:01:42 +0100

    graphic enhancements

--- chat_manager.py
+++ chat_manager.py
@@ -6,21 +6,23 @@ write = stdout.write
 class Chat_manager:
     def __init__(self) -> None:
         self.width = get_terminal_size(0)[0]
-        self.message_width = (self.width // 2) - 10
+        self.message_width = (self.width // 2) - 5
     
     def clear(self) -> None:
         write("\033[F")
 
     def get_user_message(self, msg:str) -> str:
         left_pad = self.width - self.message_width
-        left_pad_str = ''.join([' ' for _ in range(left_pad)])
+        left_pad_str = ''.join([' ' for _ in range(left_pad - 2)])
         lines = msg.split('\n')
         true_lines = []
         for line in lines:
-            true_lines += self.__split_string(line, self.message_width)
+            true_lines += self.__split_string(line, self.message_width - 4)
         for i in range(len(true_lines)):
             line_length = len(true_lines[i])
-            true_lines[i] = left_pad_str + true_lines[i] + ''.join(' ' for _ in range(line_length))
+            true_lines[i] = left_pad_str + '| ' + true_lines[i] + ''.join([' ' for _ in range(self.message_width - line_length - 4)]) + ' |'
+        border = f'|{"-" * (self.message_width -2)}|'
+        true_lines = [left_pad_str + border] + true_lines + [left_pad_str + border]
         return '\n'.join(true_lines)
     
     def __split_string(self, string:str, width:int):
@@ -42,13 +44,15 @@ class Chat_manager:
 
     def get_console_message(self, msg:str) -> str:
         right_pad = self.width - self.message_width
-        right_pad_str = ''.join([' ' for _ in range(right_pad)])
+        right_pad_str = ''.join([' ' for _ in range(right_pad - 2)])
         lines = msg.split('\n')
         true_lines = []
         for line in lines:
-            true_lines += self.__split_string(line, self.message_width)
+            true_lines += self.__split_string(line, self.message_width - 4)
         for i in range(len(true_lines)):
             line_length = len(true_lines[i])
-            true_lines[i] = true_lines[i] + ''.join(' ' for _ in range(line_length)) + right_pad_str
+            true_lines[i] = '| ' + true_lines[i] + ''.join([' ' for _ in range(self.message_width - line_length - 4)]) + ' |' + right_pad_str
+        border = f'|{"-" * (self.message_width -2)}|'
+        true_lines = [border] + true_lines + [border]
         return '\n'.join(true_lines)
 
