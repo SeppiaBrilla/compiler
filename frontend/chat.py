81d54217bab694f0e7671c28e7ca54f11bbbcf0f
Alessio Pellegrino <seppiabrilla@pop-os.lan>
2023-12-04 00:41:05 +0100

    updated frontend

--- frontend/chat.py
+++ frontend/chat.py
@@ -3,13 +3,14 @@ from sys import stdout
 
 write = stdout.write
 
-class Chat_manager:
+class Formatter:
     def __init__(self) -> None:
         self.width = get_terminal_size(0)[0]
         self.message_width = (self.width // 2) - 5
     
     def clear(self) -> None:
         write("\033[F")
+        print(" " * self.width, end="\r")
 
     def get_user_message(self, msg:str) -> str:
         left_pad = self.width - self.message_width
@@ -19,7 +20,7 @@ class Chat_manager:
         for line in lines:
             true_lines += self.__split_string(line, self.message_width - 4)
         for i in range(len(true_lines)):
-            line_length = len(true_lines[i])
+            line_length = len(self.string_decolor(true_lines[i]))
             true_lines[i] = left_pad_str + '| ' + true_lines[i] + ''.join([' ' for _ in range(self.message_width - line_length - 4)]) + ' |'
         border = f'|{"-" * (self.message_width -2)}|'
         true_lines = [left_pad_str + border] + true_lines + [left_pad_str + border]
@@ -50,9 +51,65 @@ class Chat_manager:
         for line in lines:
             true_lines += self.__split_string(line, self.message_width - 4)
         for i in range(len(true_lines)):
-            line_length = len(true_lines[i])
+            line_length = len(self.string_decolor(true_lines[i]))
             true_lines[i] = '| ' + true_lines[i] + ''.join([' ' for _ in range(self.message_width - line_length - 4)]) + ' |' + right_pad_str
         border = f'|{"-" * (self.message_width -2)}|'
         true_lines = [border] + true_lines + [border]
         return '\n'.join(true_lines)
 
+    def string_decolor(self, string):
+        if not CEND in string:
+            return string
+        colors = [CBOLD, CITALIC, CURL, CBLINK, CBLINK2, CSELECTED, CBLACK, CRED, CGREEN, CYELLOW, 
+                  CBLUE, CVIOLET, CBEIGE, CWHITE, CBLACKBG, CREDBG, CGREENBG, CYELLOWBG, CBLUEBG, 
+                  CVIOLETBG, CBEIGEBG, CWHITEBG, CGREY, CRED2, CGREEN2, CYELLOW2, CBLUE2, CVIOLET2, 
+                  CBEIGE2, CWHITE2]
+
+        for color in colors:
+            if color in string:
+                string = string.replace(color, '')
+
+        return string.replace(CEND, '')
+
+
+class Color:
+    def __init__(self, color) -> None:
+        self.color = color
+
+    def __call__(self, msg:str) -> str:
+        return f'{self.color}{msg}{CEND}'
+
+CEND      = '\33[0m'
+CBOLD     = '\33[1m'
+CITALIC   = '\33[3m'
+CURL      = '\33[4m'
+CBLINK    = '\33[5m'
+CBLINK2   = '\33[6m'
+CSELECTED = '\33[7m'
+
+CBLACK  = '\33[30m'
+CRED    = '\33[31m'
+CGREEN  = '\33[32m'
+CYELLOW = '\33[33m'
+CBLUE   = '\33[34m'
+CVIOLET = '\33[35m'
+CBEIGE  = '\33[36m'
+CWHITE  = '\33[37m'
+
+CBLACKBG  = '\33[40m'
+CREDBG    = '\33[41m'
+CGREENBG  = '\33[42m'
+CYELLOWBG = '\33[43m'
+CBLUEBG   = '\33[44m'
+CVIOLETBG = '\33[45m'
+CBEIGEBG  = '\33[46m'
+CWHITEBG  = '\33[47m'
+
+CGREY    = '\33[90m'
+CRED2    = '\33[91m'
+CGREEN2  = '\33[92m'
+CYELLOW2 = '\33[93m'
+CBLUE2   = '\33[94m'
+CVIOLET2 = '\33[95m'
+CBEIGE2  = '\33[96m'
+CWHITE2  = '\33[97m'
--- frontend/main.py
+++ frontend/main.py
@@ -1,24 +1,72 @@
-from managers.chat_manager import Chat_manager
-from compilerConnect import LLM_comunincation
-from sys import argv
+from chat import CRED, CVIOLET, Formatter, Color, CGREEN
+from re import findall
+from comunication import LLM_comunincation
+from sys import argv, exit
+from signal import signal, SIGINT
 
+def exit_signal_handler(sig, frame):
+    print("\nbye!")
+    exit(0)
 
-chat_manager = Chat_manager()
+formatter = Formatter()
+signal(SIGINT, exit_signal_handler)
+def response_parser(response, data):
+    response = response.replace("'", "")
+    response = response.replace('"', '')
+    functions = findall('\\`\\`\\`([a-zA-Z0-9_ \\-\\.,]*\\([a-zA-Z0-9_ /\\-\\.,\\[\\]]*\\))\\`\\`\\`',response)
+    plugins_values = []
+    plugins_names = []
+    color_codes = {'success': Color(CGREEN), 'error': Color(CRED), 'none': Color(CVIOLET)}
+    color = lambda msg, outcome: color_codes[outcome](msg) if outcome in color_codes else color_codes['none'](msg)
+    for function in functions:
+        split_function = function[:-1].split('(') 
+        plugins_names.append(split_function[0])
+        result = data[split_function[0]].pop(0)
+        response = response.replace('```' + function + '```', f'[{len(plugins_names) - 1}]{color(split_function[0], result["outcome"])}')
+        plugins_values.append(result['msg'])
+    return response, plugins_values, plugins_names
+
+def print_actions(plugins_names, plugins_values, response):
+    if len(plugins_names) == 0:
+        return
+    plugins_names += ["reprint the message", "query the llm"]
+    plugins_values += [response]
+    possible_values = "choose an action: " + ' '.join([f"[{i}]{plugins_names[i]}" for i in range(len(plugins_names))]) + " "
+    while True:
+        n = -1
+        while n == -1 or n > len(plugins_values):
+            try:
+                res = input(possible_values)
+                n = int(res)
+            except KeyboardInterrupt as _:
+                return
+            except Exception as _:
+                pass
+            formatter.clear()
+        if n == len(plugins_values):
+            return
+        print(formatter.get_console_message(plugins_values[n]))
 
 
 def main(port:int):
     com = LLM_comunincation(port)
 
     while True:
-        msg = input('query the llm: ')
+        msg = input('query the llm. If you want to exit write "exit()" or press ctrl + C: ')
         if 'exit()' in msg:
-            break
-        chat_manager.clear()
-        print(chat_manager.get_user_message(msg))
+            exit_signal_handler("","")
+        formatter.clear()
+        print(formatter.get_user_message(msg))
         print()
         response = com.query_llm(msg, 'fe')
-        print(chat_manager.get_console_message(response))
+        data = response['data']
+        ai_response = response['response']
+        string_response, plugins_values, plugins_names = response_parser(ai_response, data)
+        print(formatter.get_console_message(string_response))
         print()
-
+        print_actions(plugins_names, plugins_values, string_response)
+        
 if __name__ == '__main__':
     main(int(argv[1]))
+
+
